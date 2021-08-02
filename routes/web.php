05986e7d076654c688f2080b9c5e9609128f4e05
Shakil Hossain <sh9743441@gmail.com>
2021-08-02 21:19:17 +0600

    the basics

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
 });
+
+Route::get('/posts/{slug}', function ($slug) {
+    // Find a post by it's slug and pass it to a view clalled 'post'
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+})->where('post', '[A-z_\-]+');
