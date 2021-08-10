a291784c716894bd3918bbb37cd93f92c8d08284
Shakil Hossain <sh9743441@gmail.com>
2021-08-10 14:53:50 +0600

    newsletter with service container

--- app/Http/Controllers/NewsletterController.php
+++ app/Http/Controllers/NewsletterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\MailchimpNewsletter;
 use App\Services\Newsletter;
 use Illuminate\Http\Request;
 use Illuminate\Validation\ValidationException;
